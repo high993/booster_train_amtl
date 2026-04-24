ddf99db4f7839a94005e3f51983fcf5736607e3f
bmds1 <kwoktszyi1122@gmail.com>
2026-04-24 13:00:19 -0400

    Add local AMTL training stack

---
+++ scripts/rsl_rl/amtl/__init__.py
@@ -0,0 +1,1 @@
+"""Local AMTL training components."""
---
+++ scripts/rsl_rl/amtl/modules/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021-2025, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Definitions for neural-network components for RL-agents."""
+
+from amtl.actor_critic import ActorCritic
+from .actor_critic_recurrent import ActorCriticRecurrent
+from .rnd import RandomNetworkDistillation, resolve_rnd_config
+from .symmetry import resolve_symmetry_config
+
+__all__ = [
+    "ActorCritic",
+    "ActorCriticRecurrent",
+    "RandomNetworkDistillation",
+    "resolve_rnd_config",
+    "resolve_symmetry_config",
+]
